75d1fd1da0875ce3ae845874a5f7b02168d05bf8
Raymond Lam <raymond.m.lam@gmail.com>
2022-12-21 00:49:15 +0100

    changed eslint settings, tsconfig for imports and locale settings

---
+++ web/.eslintrc.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-extra-semi': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
