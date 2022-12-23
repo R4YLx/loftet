641b71d9c8e4109dbf921f5feb179f4ea0b8f177
Raymond Lam <raymond.m.lam@gmail.com>
2022-12-23 12:05:32 +0100

    created schemas for category, subcategory, product and size

---
+++ cms/schemas/product.ts
@@ -0,0 +1,81 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: '_id',
+        maxLength: 96
+      }
+    }),
+    defineField({
+      name: 'size',
+      title: 'Size',
+      type: 'reference',
+      to: [{ type: 'size' }]
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number'
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    }),
+    defineField({
+      name: 'category',
+      title: 'Category',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'category' }]
+        }
+      ]
+    }),
+    defineField({
+      name: 'subcategory',
+      title: 'Subcategory',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'subcategory' }]
+        }
+      ]
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'blockContent'
+    })
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'image'
+    },
+    prepare(selection) {
+      const { author } = selection
+      return { ...selection, subtitle: author && `by ${author}` }
+    }
+  }
+})
