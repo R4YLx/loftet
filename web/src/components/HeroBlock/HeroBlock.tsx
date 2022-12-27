0d7cdc933903ad3ef0849d3457df7906f4f1b035
Raymond Lam <raymond.m.lam@gmail.com>
2022-12-27 15:33:59 +0100

    composing HeroBlock props and elements

--- web/src/components/HeroBlock/HeroBlock.tsx
+++ web/src/components/HeroBlock/HeroBlock.tsx
@@ -1,7 +1,21 @@
+import Button from '@components/Button'
+import Image from '@components/Image'
+import Text from '@components/Text'
 import styles from './HeroBlock.module.scss'
+import { IHeroBlock } from './HeroBlock.types'
 
-const HeroBlock = () => {
-  return <div className={styles.Root}></div>
+const HeroBlock = ({ imageSrc, btnText }: IHeroBlock) => {
+  return (
+    <div className={styles.Root}>
+      <Image src={imageSrc} className={styles.Root__image} />
+
+      <Button className={styles.Root__button}>
+        <Text element="p" size="md">
+          {btnText}
+        </Text>
+      </Button>
+    </div>
+  )
 }
 
 export default HeroBlock
---
+++ web/src/components/HeroBlock/HeroBlock.types.ts
@@ -0,0 +1,4 @@
+export interface IHeroBlock {
+  imageSrc: string
+  btnText: string
+}
