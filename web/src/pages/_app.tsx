c9190024c5052c259f8941331be820c95ee7e551
Raymond Lam <raymond.m.lam@gmail.com>
2022-12-28 11:16:21 +0100

    created Modal for hamburger menu by creating portal

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useState } from 'react'
+
 import Footer from '@components/Footer'
 import Navbar from '@components/Navbar'
 import '@styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <>
       <Head>
