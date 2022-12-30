aa2b3564435ee086679487f42be72d10ccce7b37
Raymond Lam <raymond.m.lam@gmail.com>
2022-12-30 10:04:24 +0100

    fetching single product

--- web/src/pages/product/[slug].tsx
+++ web/src/pages/product/[slug].tsx
@@ -1,7 +1,48 @@
+import { sanityClient } from '@lib/sanity.config'
+import { GetStaticProps, GetStaticPropsResult } from 'next'
+import { groq } from 'next-sanity'
+import { fetchProductById } from 'utils/fetchProductById'
 import styles from './ProductPage.module.scss'
 
-const ProductPage = () => {
+const ProductPage = ({ product }: PageProps) => {
+  console.log('product', product)
+
   return <div className={styles.Root}>ProductPage</div>
 }
 
 export default ProductPage
+
+export async function getStaticPaths() {
+  const pathQuery = groq`*[_type == "products"]{
+    'slug': slug.current
+  }`
+
+  const products: Product[] = await sanityClient.fetch(pathQuery)
+
+  if (!products) {
+    return null
+  }
+
+  return {
+    paths:
+      products.map((product) => ({
+        params: {
+          slug: product.slug
+        }
+      })) || [],
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps<PageProps> = async ({ params }) => {
+  console.log('params', params) // { slug: '0a4c01ab-a577-481e-bf69-a8e167326736' }
+  console.log('params', params?.slug) //  '0a4c01ab-a577-481e-bf69-a8e167326736'
+
+  const product = await fetchProductById(String(params!.slug))
+
+  return {
+    props: {
+      product
+    }
+  }
+}
--- web/src/utils/fetchProductById.ts
+++ web/src/utils/fetchProductById.ts
@@ -1,10 +1,8 @@
 import { groq } from 'next-sanity'
 import { sanityClient } from '@lib/sanity.config'
 
-export const fetchProductById = async (productId: string) => {
-  const query = groq`*[_type == "products" && _id == ${productId}]{
-  ...
-  }`
+export const fetchProductById = async (slug: string) => {
+  const query = groq`*[_type == "products" && _id == "${slug}"][0]`
 
   const product: Product = await sanityClient.fetch(query)
 
