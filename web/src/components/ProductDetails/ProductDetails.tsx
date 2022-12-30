76f929a97d24f411c8aa4fe88ca967ea804768e8
Raymond Lam <raymond.m.lam@gmail.com>
2022-12-31 00:49:30 +0100

    moved list to own component

--- web/src/components/ProductDetails/ProductDetails.tsx
+++ web/src/components/ProductDetails/ProductDetails.tsx
@@ -1,12 +1,10 @@
 import { urlFor } from '@lib/sanity.config'
-import { textToArray } from 'helpers/textToArray'
 import Button from '@components/Button'
-import Divider from '@components/Divider'
 import Headline from '@components/Headline'
 import Image from '@components/Image'
 import Text from '@components/Text'
 import SelectMenu from '@components/SelectMenu'
-
+import ProductDetailsList from '@components/ProductDetailsList'
 import { ProductDetailsProps } from './ProductDetails.types'
 import styles from './ProductDetails.module.scss'
 
@@ -48,73 +46,7 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           </Button>
         </div>
 
-        <div className={styles.Root__detailsWrapper}>
-          <Divider className={styles.Root__mobileDivider} />
-
-          <Headline
-            element="h4"
-            size="sm"
-            className={styles.Root__detailsHeadline}
-          >
-            Product details
-          </Headline>
-
-          <Divider className={styles.Root__mdScreenDivider} />
-
-          <div className={styles.Root__listWrapper}>
-            <div className={styles.Root__listWrapper__item}>
-              <Text element="p" size="lg">
-                Item:
-              </Text>
-              <Text element="p" size="lg">
-                {product.item}
-              </Text>
-            </div>
-
-            <div className={styles.Root__listWrapper__item}>
-              <Text element="p" size="lg">
-                Material:
-              </Text>
-              <Text element="p" size="lg">
-                {product.material}
-              </Text>
-            </div>
-
-            <div className={styles.Root__listWrapper__item}>
-              <Text element="p" size="lg">
-                Color:
-              </Text>
-              <Text element="p" size="lg">
-                {product.color}
-              </Text>
-            </div>
-
-            <div className={styles.Root__listWrapper__item}>
-              <Text element="p" size="lg">
-                Condition:
-              </Text>
-              <Text element="p" size="lg">
-                {product.condition}
-              </Text>
-            </div>
-
-            {product.measurements && (
-              <div className={styles.Root__listWrapper__item}>
-                <Text element="p" size="lg">
-                  Measurements:
-                </Text>
-
-                {textToArray(product.measurements).map((length, i) => (
-                  <Text element="p" size="lg" key={i}>
-                    {length}
-                  </Text>
-                ))}
-              </div>
-            )}
-          </div>
-
-          <Divider className={styles.Root__mobileDivider} />
-        </div>
+        <ProductDetailsList product={product} />
       </div>
     </div>
   )
