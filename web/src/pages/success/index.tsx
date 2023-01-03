f6e7d70dd28d3bba1a55b7f05d7cda1a67024c19
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-03 22:01:27 +0100

    finished success page

--- web/src/pages/success/index.tsx
+++ web/src/pages/success/index.tsx
@@ -1,39 +1,96 @@
 import { GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
 import { fetchStripeLineItems } from '@utils/fetchStripeLineItems'
+import { fetchStripeCustomerDetails } from '@utils/fetchStripeCustomerDetails'
 import Headline from '@components/Headline'
 import Divider from '@components/Divider'
-import styles from './SuccessPage.module.scss'
 import Text from '@components/Text'
-import { fetchStripeCustomerDetails } from '@utils/fetchStripeCustomerDetails'
+import Table from '@components/Table'
+import styles from './SuccessPage.module.scss'
+import Button from '@components/Button'
 
 const SuccessPage = ({ lineItems, customer }: PageProps) => {
-  console.log('lineItems', lineItems)
-  console.log('customer', customer)
-
   const router = useRouter()
   const { session_id } = router.query
-  const orderNumber = session_id?.slice(-5)
+  const orderNumber = session_id?.slice(-10)
 
   return (
     <div className={styles.Root}>
       <header className={styles.Root__header}>
-        <Headline element="h2" size="xl">
+        <Headline element="h2" size="xl" className={styles.Root__headline}>
           It&apos;s ordered!
         </Headline>
 
         <Text element="p" size="xl">
-          Thank you for your order, we hope you enjoyed shopping with us.
+          Hi {customer ? customer?.name.split(' ')[0] : 'friend'} - Thank you
+          for your order, we hope you enjoyed shopping with us.
         </Text>
       </header>
 
       <Divider subtle />
 
-      <main>
-        <section></section>
+      <main className={styles.Root__wrapper}>
+        <section className={styles.Root__orderSection}>
+          <Headline element="h4" size="sm">
+            Order number: {orderNumber}
+          </Headline>
 
-        {lineItems &&
-          lineItems.map((lineItem) => <div key={lineItem.id}></div>)}
+          <Text element="p" size="lg">
+            We&apos;ve sent the confirmation to {customer?.email}
+          </Text>
+        </section>
+
+        <section className={styles.Root__productSection}>
+          {lineItems && <Table items={lineItems} />}
+        </section>
+
+        <section className={styles.Root__deliverySection}>
+          <div className={styles.Root__textContainer}>
+            <Headline element="h4" size="sm">
+              Your order is being shipped to:
+            </Headline>
+
+            <div className={styles.Root__customerDetails}>
+              <Text element="p" size="lg">
+                {customer?.name}
+              </Text>
+
+              <Text element="p" size="lg">
+                {customer?.address.line1}
+              </Text>
+              <Text element="p" size="lg">
+                {customer?.address.postal_code} {customer?.address.city}
+              </Text>
+              <Text element="p" size="lg">
+                {customer?.address.country}
+              </Text>
+            </div>
+          </div>
+
+          <div className={styles.Root__textContainer}>
+            <Headline element="h4" size="sm">
+              Estimated time of delivery:
+            </Headline>
+
+            <Text element="p" size="lg">
+              5-7 working days
+            </Text>
+          </div>
+        </section>
+
+        <div className={styles.Root__ctaWrapper}>
+          <Button
+            block
+            bgDark
+            isFluid
+            onClick={() => router.push('/', undefined, { shallow: true })}
+            className={styles.Root__CTA}
+          >
+            <Text element="p" size="lg">
+              Continue shopping
+            </Text>
+          </Button>
+        </div>
       </main>
     </div>
   )
