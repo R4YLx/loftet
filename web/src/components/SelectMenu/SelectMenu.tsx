0fc51f4e8e019add524a6aed127f1f7208dda04b
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-05 09:49:26 +0100

    created variant of select menu

--- web/src/components/SelectMenu/SelectMenu.tsx
+++ web/src/components/SelectMenu/SelectMenu.tsx
@@ -1,14 +1,29 @@
 import { SelectMenuProps } from './SelectMenu.types'
 import styles from './SelectMenu.module.scss'
+import clsx from 'clsx'
 
-const SelectMenu = ({ option, options, handleOptions }: SelectMenuProps) => {
+const SelectMenu = ({
+  option,
+  options,
+  handleOptions,
+  className,
+  isFluid,
+  sizeSelection
+}: SelectMenuProps) => {
+  const selectStyles = clsx(
+    styles.Root,
+    className,
+    isFluid && styles.Root_isFluid,
+    sizeSelection && styles.Root_sizeSelection
+  )
+
   if (options && handleOptions) {
     const orderBy = (e: React.ChangeEvent<HTMLSelectElement>) => {
       handleOptions(e.target.value)
     }
 
     return (
-      <select className={styles.Root} onChange={orderBy}>
+      <select className={selectStyles} onChange={orderBy}>
         {options.map((option, i) => (
           <option key={i} value={option.value}>
             {option.label}
@@ -19,7 +34,7 @@ const SelectMenu = ({ option, options, handleOptions }: SelectMenuProps) => {
   }
 
   return (
-    <select className={styles.Root}>
+    <select className={selectStyles}>
       <option value={option}>{option}</option>
     </select>
   )
