e6eddea5a8701b3ff06b67ae80a202e6ea2ac42e
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-10 00:34:03 +0100

    Can sort searched items

--- web/src/pages/search/index.tsx
+++ web/src/pages/search/index.tsx
@@ -1,19 +1,62 @@
+import { useMemo } from 'react'
 import { useRouter } from 'next/router'
 import { useSearchProducts } from 'hooks/useSearchProducts'
+import {
+  setDefault,
+  sortHighToLow,
+  sortLowToHigh,
+  sortNewToOld
+} from '@utils/helpers'
 import Headline from '@components/Headline'
 import ProductsGrid from '@components/ProductsGrid'
 import ProductCard from '@components/ProductCard'
-
-import styles from './SearchPage.module.scss'
 import Text from '@components/Text'
+import SelectMenu from '@components/SelectMenu'
+import styles from './SearchPage.module.scss'
+
+const options = [
+  { value: '', label: '' },
+  { value: 'low', label: 'Lowest to highest price' },
+  { value: 'high', label: 'Highest to lowest price' },
+  { value: 'new', label: 'Newest arrivals' }
+]
 
 const SearchPage = () => {
   const router = useRouter()
   const query = String(router.query.result)
+  const option = router.query.sort
 
   const { data: products } = useSearchProducts(query)
 
-  console.log('router', router)
+  // Handler for sorting
+  const handleOrderBy = (option: string) => {
+    router.push({ pathname: '/search', query: { result: query, sort: option } })
+  }
+
+  // Sorting collection
+  const sortedProducts = useMemo(() => {
+    if (!products) return
+
+    switch (option) {
+      default: {
+        const copy = setDefault(products)
+        return copy
+      }
+      case 'low': {
+        const copy = sortLowToHigh(products)
+        return copy
+      }
+      case 'high': {
+        const copy = sortHighToLow(products)
+        return copy
+      }
+
+      case 'new': {
+        const copy = sortNewToOld(products)
+        return copy
+      }
+    }
+  }, [option, products, router.query.slug])
 
   return (
     <div className={styles.Root}>
@@ -27,12 +70,28 @@ const SearchPage = () => {
         </Text>
       </div>
 
-      <ProductsGrid>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
-      </ProductsGrid>
+      <main>
+        <div className={styles.Root__selectContainer}>
+          <Text element="p" size="md" className={styles.Root__selectLabel}>
+            Sort by:
+          </Text>
+
+          <div className={styles.Root__selectWrapper}>
+            <SelectMenu
+              options={options}
+              option={String(option)}
+              handleOptions={handleOrderBy}
+            />
+          </div>
+        </div>
+
+        <ProductsGrid>
+          {sortedProducts &&
+            sortedProducts.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+        </ProductsGrid>
+      </main>
     </div>
   )
 }
