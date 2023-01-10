75245aa8aa4f7f89e03ae0e6d53661234268be07
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-10 11:24:34 +0100

    cleaning up unused code

--- web/src/hooks/useSearchProducts.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { fetchSearchedProducts } from '@utils/sanityAPI'
-
-export const useSearchProducts = (query: string) => {
-  return useQuery<IProduct[]>(['products', query], () =>
-    fetchSearchedProducts(query)
-  )
-}
