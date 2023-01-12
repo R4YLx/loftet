9564143771e41e788c07ed7e89d54fa4c2bf58fc
Raymond Lam <raymond.m.lam@gmail.com>
2023-01-12 17:57:05 +0100

    installed react testing library and jest

---
+++ web/jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+const customJestConfig = {
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@lib/(.*)$': '<rootDir>/src/lib//$1'
+  },
+  testEnvironment: 'jest-environment-jsdom'
+}
+
+module.exports = createJestConfig(customJestConfig)
